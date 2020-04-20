134da973e4978fdc6237c9a6a009b8ce63e8120b
jaffarabbas <gamaportal8@gmail.com>
2020-04-20 22:57:53 +0500

    .

---
+++ first.java
@@ -0,0 +1,30 @@
+
+
+abstract class ones{
+    abstract void one1();
+    abstract void one2();
+    abstract void one3();
+}
+
+class child extends ones{
+    void one1(){
+        System.out.println("hello");
+    }
+    void one2(){
+        System.out.println("hello");
+    }
+    void one3(){
+        System.out.println("hello");
+    }
+}
+
+
+class first{
+    public static void main(String[] args) {
+        child obj = new child();
+
+        obj.one1();
+        obj.one2();
+        obj.one3();
+    }
+}
